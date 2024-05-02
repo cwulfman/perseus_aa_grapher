e6b394b2115e47007eb26851160a2daf350252a9
Cliff Wulfman <cwulfman@princeton.edu>
2024-05-02 11:17:22 -0400

    testing models

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,3 @@
-"""
-    Dummy conftest.py for pdl_importer.
-
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    - https://docs.pytest.org/en/stable/fixture.html
-    - https://docs.pytest.org/en/stable/writing_plugins.html
-"""
-
 import pytest
 import json
 
@@ -14,4 +5,10 @@ import json
 @pytest.fixture
 def artifact_test_data():
     with open('tests/data/artifacts.json') as f:
+        return json.load(f)
+
+
+@pytest.fixture
+def vase_test_data():
+    with open('tests/data/vases.json') as f:
         return json.load(f)
--- tests/test_models.py
+++ tests/test_models.py
@@ -7,5 +7,10 @@ __copyright__ = "Cliff Wulfman"
 __license__ = "MIT"
 
 
-def test_vase_data(artifact_test_data):
-    assert artifact_test_data['object'][0]['xml:id'] == "aa_1"
+def test_vase_data(vase_test_data):
+    assert vase_test_data['object'][0]['id'] == "aa_3951"
+
+
+def test_vase_data_object(vase_test_data):
+    a_vase = VaseData(**vase_test_data['object'][0])
+    assert a_vase.name == "Malibu 86.AE.34"
